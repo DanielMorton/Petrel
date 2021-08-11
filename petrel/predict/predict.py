2990f024496fdae5fec341f8bf7b6e566af563c3
Daniel  Morton <dacmorton@gmail.com>
2021-08-11 15:03:19 -0400

    Fix device

--- petrel/predict/predict.py
+++ petrel/predict/predict.py
@@ -26,11 +26,14 @@ def make_prediction(model, images, targets=None):
     return pd.DataFrame(predictions)
 
 
-def make_prediction_df(model, test_loader, verbose=0):
+def make_prediction_df(model, test_loader,
+                       device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+                       verbose=0):
     """
     Creates a Dataframe of predictions from unlabeled test data.
     :param model: The model used for predictions.
     :param test_loader: Dataloader for unlabeled test data.
+    :param device: Device to load the model on. CPU or CUDA. Defaults using CUDA if available, otherwise CPU.
     :param verbose: If positive prints output every "verbose" steps.
     :return: Dataframe of bounding box predictions.
     """
@@ -39,19 +42,22 @@ def make_prediction_df(model, test_loader, verbose=0):
     n = 0
     for images in test_loader:
         n += 1
-        images = torch.stack(images).cuda().float()
-        predictions.append(make_prediction(model, images, labels))
+        images = torch.stack(images).to(device).float()
+        predictions.append(make_prediction(model, images))
         if verbose and n % verbose == 0:
             print(f"Processed batch {n}.")
 
     return pd.concat(predictions).reset_index(drop=True)
 
 
-def make_val_prediction_df(model, test_loader, verbose=0):
+def make_val_prediction_df(model, test_loader,
+                           device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+                           verbose=0):
     """
     Creates a Dataframe of predictions from labeled data.
     :param model: The model used for predictions.
     :param test_loader: Dataloader for unlabeled test data.
+    :param device: Device to load the model on. CPU or CUDA. Defaults using CUDA if available, otherwise CPU.
     :param verbose: If positive prints output every "verbose" steps.
     :return: Dataframe of predicted and ground truth bounding box labels.
     """
@@ -60,8 +66,8 @@ def make_val_prediction_df(model, test_loader, verbose=0):
     n = 0
     for images, labels in test_loader:
         n += 1
-        images = torch.stack(images).cuda().float()
-        predictions.append(make_prediction(model, images))
+        images = torch.stack(images).to(device).float()
+        predictions.append(make_prediction(model, images, labels))
         if verbose and n % verbose == 0:
             print(f"Processed batch {n}.")
 
