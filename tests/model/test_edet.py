4b6487642219408fa1dafacc4c8e4f277e61be8c
Daniel  Morton <dacmorton@gmail.com>
2021-08-13 16:54:27 -0400

    Add DetBenchTrainVal to return loss without detection during evaluation.

--- tests/model/test_edet.py
+++ tests/model/test_edet.py
@@ -1,7 +1,7 @@
 from effdet.bench import DetBenchPredict, DetBenchTrain
 from unittest import TestCase
 
-from petrel.model import load_edet
+from petrel.model import load_edet, DetBenchTrainVal
 
 
 class TestEnet(TestCase):
@@ -14,7 +14,7 @@ class TestEnet(TestCase):
         assert model.max_det_per_image == 1000
         assert model.max_detection_points == 5000
         assert not model.soft_nms
-        assert model.__class__ == DetBenchTrain
+        assert model.__class__ == DetBenchTrainVal
 
     def test_det_per_image(self):
         model = load_edet(config_name="tf_efficientdet_d0",
@@ -25,7 +25,7 @@ class TestEnet(TestCase):
         assert model.max_det_per_image == 100
         assert model.max_detection_points == 5000
         assert not model.soft_nms
-        assert model.__class__ == DetBenchTrain
+        assert model.__class__ == DetBenchTrainVal
 
     def test_enet(self):
         model = load_edet(config_name="tf_efficientdet_d0",
@@ -35,7 +35,7 @@ class TestEnet(TestCase):
         assert model.max_det_per_image == 1000
         assert model.max_detection_points == 5000
         assert not model.soft_nms
-        assert model.__class__ == DetBenchTrain
+        assert model.__class__ == DetBenchTrainVal
 
     def test_image_int(self):
         model = load_edet(config_name="tf_efficientdet_d0",
@@ -46,7 +46,7 @@ class TestEnet(TestCase):
         assert model.max_det_per_image == 1000
         assert model.max_detection_points == 5000
         assert not model.soft_nms
-        assert model.__class__ == DetBenchTrain
+        assert model.__class__ == DetBenchTrainVal
         assert model.config.image_size == [512, 512]
 
     def test_image_size(self):
@@ -58,7 +58,7 @@ class TestEnet(TestCase):
         assert model.max_det_per_image == 1000
         assert model.max_detection_points == 5000
         assert not model.soft_nms
-        assert model.__class__ == DetBenchTrain
+        assert model.__class__ == DetBenchTrainVal
         assert model.config.image_size == [256, 256]
 
     def test_soft_nms(self):
@@ -70,7 +70,7 @@ class TestEnet(TestCase):
         assert model.max_det_per_image == 1000
         assert model.max_detection_points == 5000
         assert model.soft_nms
-        assert model.__class__ == DetBenchTrain
+        assert model.__class__ == DetBenchTrainVal
 
     def test_train(self):
         model = load_edet(config_name="tf_efficientdet_d0",
@@ -90,6 +90,17 @@ class TestEnet(TestCase):
                           checkpoint_path="tests/tf_efficientdet_d1_40-a30f94af.pth",
                           train=True)
         assert model.num_classes == 90
+        assert model.num_levels == 5
+        assert model.max_det_per_image == 1000
+        assert model.max_detection_points == 5000
+        assert not model.soft_nms
+        assert model.__class__ == DetBenchTrainVal
+
+    def test_detection(self):
+        model = load_edet(config_name="tf_efficientdet_d0",
+                          image_size=(512, 512),
+                          detection=True)
+        assert model.num_classes == 1
         assert model.num_levels == 5
         assert model.max_det_per_image == 1000
         assert model.max_detection_points == 5000
